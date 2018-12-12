3941ee41b5fb8f33ce1a39280882abeaf4fd413b
Megga-Miister <mtriplett@ISOLALT-64.localdomain>
2018-12-12 09:19:16 -0800

    feature 2 wip

--- server.js
+++ server.js
@@ -2,11 +2,15 @@
 
 const express = require('express');
 const ejs = require('ejs');
+const superagent = require('superagent');
 const cors = require('cors');
 
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.set('view engine', 'ejs');
 
@@ -16,10 +20,20 @@ app.get('/', (req, res) => {
   res.render('../public/views/pages/index', { greeting: greetingMessage });
 });
 
-//TODO: double check this is set up correctly
-app.get('/show', (req, res) => {
-  res.render('../public/views/pages/searches/show',);
-});
+// //TODO: double check this is set up correctly
+// app.get('/show', (req, res) => {
+//   res.render('../public/views/pages/searches/show',);
+// });
+
+// TODO: finish out setting up API query, should this be in index.ejs?
+
+// helper function
+function getBook (req, res) {
+  superagent.get('https://www.googleapis.books/v1/volumes?q=${request.queryType}:${request.query}')
+  .then(result => {
+      const
+  })
+}
 
 //this is here for when we want to ejs a header into index
 // app.get('/header', (req, res) => {
@@ -30,7 +44,6 @@ app.get('/show', (req, res) => {
 app.post('/show', (req, res) => {
   res.render('../public/views/pages/searches/show',);
 });
-
 
 app.listen(PORT, () => {
   console.log(`listening on port: ${PORT}`)});
